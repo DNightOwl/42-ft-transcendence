31abbb24faf9c02fddd32508a5f408bb56e8e007
MustaphaOuassit <mustaphaouassit60@gmail.com>
2023-01-22 00:17:05 +0100

    clear branch

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-
-npm run db:push
-npm run start:dev
--- src/server/auth/dto/types.ts
+++
@@ -1,8 +0,0 @@
-import { AuthDto } from "./auth.dto";
-
-export type RequestWithUser = Request & { user: AuthDto | undefined | null };
-
-export type jwtPayload = {
-	login : string,
-	email : string
-}
