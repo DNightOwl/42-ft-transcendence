b7b3fa8e803a8661c8f46d128333a74c21c0ac8b
Mustapha Ouassit <mouassit@e1r11p3.1337.ma>
2023-01-25 14:12:39 +0100

    responsive layout home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   });
   
   return (
-    <main className='flex flex-col gap-5'>
+    <main className='flex flex-col gap-5 2xl:container 2xl:mx-auto 2xl:bg-slate-400'>
         <h1 className='text-primaryText text-2xl flex items-center gap-1.5'><span>Live Games</span><img src={fire} alt="fire"  className='w-4'/></h1>
         <section className='flex flex-col gap-6 lg:flex-row lg:justify-between lg:items-start'>
           <button className='w-full h-96 bg-shape rounded-xl lg:flex-1 lg:h-full'></button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
       },
       fontFamily: {
         sans: ['Poppins'],
-      }
+      },
     },
   },
   plugins: [],
