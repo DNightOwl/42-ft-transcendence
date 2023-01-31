875b64b2015ff9259fb033dde1c3f56e412ea607
MustaphaOuassit <mustaphaouassit60@gmail.com>
2023-01-31 21:05:35 +0100

    fix scroll bar in view home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,16 @@ import fire from '../assets/fire.png';
 export default function Home() {
   useEffect(()=>{
     document.title = "Pong - Home";
-  });
+    var scroll = document.getElementById('scroll');
+    console.log(scroll);
+    
+    if(scroll)
+    {
+      var hasVerticalScrollbar = scroll.scrollHeight > scroll.clientHeight;
+      if(hasVerticalScrollbar)
+        scroll.classList.add("lg:pr-6")
+    }
+  },[]);
   
   return (
     <main>
@@ -12,7 +21,7 @@ export default function Home() {
         <h1 className='text-primaryText text-2xl flex items-center gap-1.5'><span>Live Games</span><img src={fire} alt="fire"  className='w-4'/></h1>
         <section className='flex flex-col gap-6 lg:flex-row lg:items-start'>
           <button className='w-full h-96 bg-shape rounded-xl lg:flex-1 primary-live'></button>
-          <div className='flex gap-4 lg:flex-col live-list overflow-auto lg:overflow-x-hidden'>
+          <div className='flex gap-4 lg:flex-col live-list overflow-auto lg:overflow-x-hidden pb-6 lg:pb-0' id='scroll'>
             <button className='w-72 h-48 bg-shape rounded-xl border-4 border-primary flex-shrink-0'></button>
             <button className='w-72 h-48 bg-shape rounded-xl flex-shrink-0'></button>
             <button className='w-72 h-48 bg-shape rounded-xl flex-shrink-0'></button>
