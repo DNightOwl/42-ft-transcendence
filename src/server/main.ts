d4a830bc19ba8c9dbf1a926e39f19d7678ce39bd
DNightOwl <laafilal@student.1337.ma>
2023-01-31 18:16:11 +0100

    adding CORS configuration to main.js
    this configuration will allow resources to be requested from another domain which in our case is the front side
    and by enabling credentials the cookies and other authentication credentials will be included in requests

--- src/server/main.ts
+++ src/server/main.ts
@@ -7,6 +7,10 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+  })
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({whitelist: true, transform: true}));
   const prismaService = app.get(PrismaService);
