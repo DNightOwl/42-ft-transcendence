a04b8af2c76b06c2de36f5b298fd3fd2c2e65483
MustaphaOuassit <mustaphaouassit60@gmail.com>
2023-01-31 21:28:45 +0100

    change id with ref

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useRef } from 'react';
 import fire from '../assets/fire.png';
 
 export default function Home() {
+  const scroll = useRef<HTMLDivElement>(null);
   useEffect(()=>{
     document.title = "Pong - Home";
-    var scroll = document.getElementById('scroll');
-    console.log(scroll);
-    
-    if(scroll)
+    if(scroll.current)
     {
-      var hasVerticalScrollbar = scroll.scrollHeight > scroll.clientHeight;
+      let hasVerticalScrollbar = scroll.current.scrollHeight > scroll.current.clientHeight;
       if(hasVerticalScrollbar)
-        scroll.classList.add("lg:pr-6")
+        scroll.current.classList.add("lg:pr-6")
     }
   },[]);
   
@@ -21,7 +19,7 @@ export default function Home() {
         <h1 className='text-primaryText text-2xl flex items-center gap-1.5'><span>Live Games</span><img src={fire} alt="fire"  className='w-4'/></h1>
         <section className='flex flex-col gap-6 lg:flex-row lg:items-start'>
           <button className='w-full h-96 bg-shape rounded-xl lg:flex-1 primary-live'></button>
-          <div className='flex gap-4 lg:flex-col live-list overflow-auto lg:overflow-x-hidden pb-6 lg:pb-0' id='scroll'>
+          <div className='flex gap-4 lg:flex-col live-list overflow-auto lg:overflow-x-hidden pb-6 lg:pb-0' ref={scroll}>
             <button className='w-72 h-48 bg-shape rounded-xl border-4 border-primary flex-shrink-0'></button>
             <button className='w-72 h-48 bg-shape rounded-xl flex-shrink-0'></button>
             <button className='w-72 h-48 bg-shape rounded-xl flex-shrink-0'></button>
