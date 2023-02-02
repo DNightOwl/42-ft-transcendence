868674c4a49e619362460a7eba53abf7e8f77286
MustaphaOuassit <mustaphaouassit60@gmail.com>
2023-02-02 13:49:20 +0100

    organize tailwind classes

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,31 +1,34 @@
-import React, { useEffect } from 'react'
-import CardProfile from './Items/CardProfile';
-
+import React, { useEffect } from "react";
+import CardProfile from "./Items/CardProfile";
 
 export default function Profile() {
-  useEffect(()=>{
+  useEffect(() => {
     document.title = "Pong - Profile";
   });
 
   return (
     <main>
-      <section className='flex justify-center flex-col items-center gap-10 lg:flex-row lg:justify-between'>
-          <CardProfile settings={true}/>
-          <div className='flex justify-around w-full bg-green-100'>
-            <span className='flex flex-col items-center flex-1'>
-              <span className='text-primaryText font-extrabold text-4xl'>10</span>
-              <span className='text-secondaryText font-light text-sm'>Friends</span>
+      <section className="flex  flex-col items-center gap-10  justify-center lg:flex-row lg:justify-between">
+        <CardProfile settings={true} />
+        <div className="flex justify-around w-full bg-green-100">
+          <span className="flex flex-col items-center flex-1">
+            <span className="text-primaryText font-extrabold text-4xl">10</span>
+            <span className="text-secondaryText font-light text-sm">
+              Friends
             </span>
-            <span className='flex flex-col items-center flex-1 border-secondaryText number-profile'>
-              <span className='text-primaryText font-extrabold text-4xl'>8</span>
-              <span className='text-secondaryText font-light text-sm'>Wins</span>
+          </span>
+          <span className="flex flex-col items-center flex-1 border-secondaryText number-profile">
+            <span className="text-primaryText font-extrabold text-4xl">8</span>
+            <span className="text-secondaryText font-light text-sm">Wins</span>
+          </span>
+          <span className="flex flex-col items-center flex-1 border-secondaryText number-profile">
+            <span className="text-primaryText font-extrabold text-4xl">0</span>
+            <span className="text-secondaryText font-light text-sm">
+              Losses
             </span>
-            <span className='flex flex-col items-center flex-1 border-secondaryText number-profile'>
-              <span className='text-primaryText font-extrabold text-4xl'>0</span>
-              <span className='text-secondaryText font-light text-sm'>Losses</span>
-            </span>
-          </div>
+          </span>
+        </div>
       </section>
     </main>
-  )
+  );
 }
