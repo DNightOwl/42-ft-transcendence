eb798d39faec4d46c413c54407c92bd6a140a6af
MustaphaOuassit <mustaphaouassit60@gmail.com>
2023-02-07 00:27:46 +0100

    add not found

--- frontend/src/components/NotFound.tsx
+++ frontend/src/components/NotFound.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className='flex justify-center text-3xl text-primaryText h-full items-center'>404 Not Found !</div>
   )
 }
