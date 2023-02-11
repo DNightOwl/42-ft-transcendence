fef47a2b911361292b49b84072fef778783c00e0
Mustapha Ouassit <mouassit@e1r12p5.1337.ma>
2023-02-11 14:37:11 +0100

    fix state

--- frontend/src/components/Items/Friends.tsx
+++ frontend/src/components/Items/Friends.tsx
@@ -23,7 +23,7 @@ export default function Friends({username}:typeProps) {
                 setFriends(res);
             })   
         }
-    },[])
+    },[username])
   return (
     <div className='flex pt-10 content-profile lg:pb-10 flex-col gap-12'>
         {/* <div className='flex w-full justify-center items-center flex-col md:justify-start md:flex-row gap-12'>
