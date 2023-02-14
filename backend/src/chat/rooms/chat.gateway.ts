136f84cf9bccbbea6d615f5a6fe5cf3c8df4b94d
houbeid <houbeid@student.1337.ma>
2023-02-15 00:39:19 +0100

    fixed: empty channel

--- backend/src/chat/rooms/chat.gateway.ts
+++ backend/src/chat/rooms/chat.gateway.ts
@@ -156,7 +156,9 @@ import {
     }
     try {
       const user = await this.roomservice.getUserFromAuthenticationToken(jwttoken);
+      client.user = user;
       const test = this.OnlineUser.find((user) => user==client.user);
+      console.log(test);
       if (!test)
       {
         console.log('=======>');
