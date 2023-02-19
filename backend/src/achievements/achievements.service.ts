61aaae7ad5942d793e57a1beb0e566456d86ec76
DNightOwl <laafilal@student.1337.ma>
2023-02-19 05:43:06 +0100

    achievements done

--- backend/src/achievements/achievements.service.ts
+++ backend/src/achievements/achievements.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Game } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 
@@ -41,17 +42,35 @@ export class AchievementsService {
 		return {Achievement : names};
 	}
 
-	// user1 game stats:{userid : xxxxxxx ,stat: "win" , score : 20}
-	// user2 game stats:{userid : xxxxxxx ,stat: "lose" , score : 4}
 	async checkAchievement(gameStat : {userid:string , stat: string , score : number}) : Promise<string[]> {
-		const winCount : number= 1;
-		const loseCount : number= 10;
+		const wonGames : {wonGames : Game[]} = await this.prisma.user.findFirst({
+			where:{
+				id : gameStat.userid,
+			},
+			select:
+			{
+				wonGames:true
+			}
+		})
+		const lostGames : {lostGames : Game[]} = await this.prisma.user.findFirst({
+			where:{
+				id : gameStat.userid,
+			},
+			select:
+			{
+				lostGames:true
+			}
+		})
+		const winCount : number= wonGames.wonGames.length + (gameStat.stat === "win" ? 1: 0);
+		const loseCount : number= lostGames.lostGames.length + (gameStat.stat === "lose" ? 1: 0);
+		console.log("win ", winCount);
+		console.log("lose ", loseCount);
 		let achievementIds : Nullable<string[]|null> = []
 		//check achievement with point
 		const craftedGoalPoints : string = await this.craftAchievementGoal(AchievementType.POINT, gameStat.score);
 		const achievementId : { id : string} = await this.fetchAchievementByGoal(craftedGoalPoints);
-		if( achievementId?.id)
-				achievementIds.push(achievementId.id);
+		if(achievementId?.id)
+			achievementIds.push(achievementId.id);
 		//if check achievement with win
 		//else check achievement with lose
 		if(gameStat.stat === "win") {
@@ -63,7 +82,7 @@ export class AchievementsService {
 		}
 		else if (gameStat.stat === "lose") {
 			//get count of loses in match history
-			const craftedGoalLoses : string = await this.craftAchievementGoal(AchievementType.LOSE, winCount);
+			const craftedGoalLoses : string = await this.craftAchievementGoal(AchievementType.LOSE, loseCount);
 			const achievementId : { id : string} = await this.fetchAchievementByGoal(craftedGoalLoses);
 			if( achievementId?.id)
 				achievementIds.push(achievementId.id);
@@ -112,13 +131,7 @@ export class AchievementsService {
 					}),
 				},
 			},
-		  });  
+		  });
 	}
 	
 }
-
-// /////testing achivements 
-// const achievementIds :string[]= await this.achi.checkAchievement({userid:user.id , stat: "win" , score : 5});
-// update the achievements relation with user
-// await this.updateUserAchievements(userid , achievementIds);
-// /////end testing achivements
